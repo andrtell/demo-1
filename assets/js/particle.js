2398f89c9307c44646e5abf9cebe237c216fba7e
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-08-07 16:47:54 +0200

    wip

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -12,26 +12,13 @@ class List {
     }
     return seq;
   }
-
-  static normalize_min_max(lst) {
-    let min = lst[0];
-    let max = lst[0];
-    for (let i = 0; i < lst.length; i++) {
-      min = Math.min(min, lst[i]);
-      max = Math.max(max, lst[i]);
-    }
-    const range = max - min;
-    if (range == 0) {
-      return values;
-    }
-    for (let i = 0; i < lst.length; i++) {
-      lst[i] = (lst[i] - min) / range;
-    }
-    return values;
-  }
 }
 
 class Interpolation {
+  static smooth(t) {
+    return t * t * (3 - 2 * t);
+  }
+
   static linear_1D(a, b, mu) {
     return a * (1 - mu) + b * mu;
   }
@@ -53,31 +40,6 @@ class Interpolation {
     const y0 = cosine_1D(a, b, x_mu);
     const y1 = cosine_1D(c, d, x_mu);
     return cosine_1D(y0, y1, y_mu);
-  }
-
-  static perlin_1D(v0, v1, mu) {
-    const mu0 = [mu, 0];
-    const mu1 = [1 - mu, 0];
-    return Interpolation.cosine_1D(
-      Vector.dot_product(v0, mu0),
-      Vector.dot_product(v1, mu1),
-      mu,
-    );
-  }
-
-  static perlin_2D(v0, v1, v2, v3, x_mu, y_mu) {
-    const mu0 = [x_mu, y_mu];
-    const mu1 = [1 - x_mu, y_mu];
-    const mu2 = [x_mu, 1 - y_mu];
-    const mu3 = [1 - x_mu, 1 - y_mu];
-    return Interpolation.cosine_2D(
-      Vector.dot_product(v0, mu0),
-      Vector.dot_product(v1, mu1),
-      Vector.dot_product(v2, mu2),
-      Vector.dot_product(v3, mu3),
-      x_mu,
-      y_mu,
-    );
   }
 }
 
@@ -165,8 +127,9 @@ class Random {
   }
 
   static discrete_vector_1D(no_samples) {
-    const PI2 = Math.PI * 2;
-    const v = Random.samples(no_samples).map((s) => Vector.unit_rad(s * PI2));
+    const v = Random.samples(no_samples).map((s) => {
+      return Vector.unit_rad((s * Math.PI) / 4);
+    });
     const p = Random.permutation_1D(no_samples);
     return (x) => {
       return v[p(x)];
@@ -174,8 +137,9 @@ class Random {
   }
 
   static discrete_vector_2D(no_samples) {
-    const PI2 = Math.PI * 2;
-    const v = Random.samples(no_samples).map((s) => Vector.unit_rad(s * PI2));
+    const v = Random.samples(no_samples).map((s) =>
+      Vector.unit_rad(s * Math.PI * 2),
+    );
     const p = Random.permutation_2D(no_samples);
     return (x, y) => {
       return v[p(x, y)];
@@ -191,23 +155,14 @@ class Random {
     };
   }
 
-  static perlin_noise_1D(no_samples) {
-    const grid = Random.discrete_vector_1D(no_samples);
-    return (x) => {
-      const x_ = Math.floor(x);
-      const mu = x - x_;
-      return Interpolation.perlin_1D(grid(x_), grid(x_ + 1), mu);
-    };
-  }
-
   static value_noise_2D(no_samples) {
     const grid = Random.discrete_number_2D(no_samples);
     return (x, y) => {
       const x_ = Math.floor(x);
       const y_ = Math.floor(y);
-      const x_mu = x - x_;
-      const y_mu = y - y_;
-      return Interpolation.cosine_2D(
+      const x_mu = Interpolation.smooth(x - x_);
+      const y_mu = Interpolation.smooth(y - y_);
+      return Interpolation.linear_2D(
         grid(x_, y_),
         grid(x_ + 1, y_),
         grid(x_, y_ + 1),
@@ -218,18 +173,32 @@ class Random {
     };
   }
 
+  static perlin_noise_1D(no_samples) {
+    const grid = Random.discrete_vector_1D(no_samples);
+    return (x) => {
+      const x_ = Math.floor(x);
+      const mu = Interpolation.smooth(x - x_);
+      return Interpolation.cosine_1D(
+        Vector.dot_product(grid(x_), [mu, 0]),
+        Vector.dot_product(grid(x_ + 1), [1 - mu, 0]),
+        mu,
+      );
+    };
+  }
+
   static perlin_noise_2D(no_samples) {
     const grid = Random.discrete_vector_2D(no_samples);
     return (x, y) => {
       const x_ = Math.floor(x);
       const y_ = Math.floor(y);
-      const x_mu = x - x_;
-      const y_mu = y - y_;
-      return Interpolation.perlin_2D(
-        grid(x_, y_),
-        grid(x_ + 1, y_),
-        grid(x_, y_ + 1),
-        grid(x_ + 1, y_ + 1),
+      const x_mu = Interpolation.smooth(x - x_);
+      const y_mu = Interpolation.smooth(y - y_);
+      const { dot_product } = Vector;
+      return Interpolation.cosine_2D(
+        dot_product(grid(x_, y_), [x_mu, y_mu]),
+        dot_product(grid(x_ + 1, y_), [1 - x_mu, y_mu]),
+        dot_product(grid(x_, y_ + 1), [x_mu, 1 - y_mu]),
+        dot_product(grid(x_ + 1, y_ + 1), [1 - x_mu, 1 - y_mu]),
         x_mu,
         y_mu,
       );
@@ -244,17 +213,18 @@ can.width = bcr.width;
 const ctx = can.getContext("2d");
 const dat = ctx.createImageData(bcr.width, bcr.height);
 
-const z = Random.perlin_noise_2D(256);
+const perl1 = Random.perlin_noise_2D(256);
+const perl2 = Random.perlin_noise_2D(256);
+const perl3 = Random.perlin_noise_2D(256);
 
 // const pix = [];
 //
-for (let x = 0; x < can.width; x++) {
-  for (let y = 0; y < can.height; y++) {
+for (let x = 0; x < 1000; x++) {
+  for (let y = 0; y < 1000; y++) {
     let idx = (x + y * dat.width) * 4;
-    const val = z(x * 0.01, y * 0.01) * 255;
-    dat.data[idx + 0] = val;
-    dat.data[idx + 1] = val;
-    dat.data[idx + 2] = val;
+    dat.data[idx + 0] = Math.abs(perl1(x * 0.05, y * 0.05) * 255);
+    dat.data[idx + 1] = Math.abs(perl2(x * 0.05, y * 0.05) * 255);
+    dat.data[idx + 2] = Math.abs(perl3(x * 0.05, y * 0.05) * 255);
     dat.data[idx + 3] = 255;
   }
 }
