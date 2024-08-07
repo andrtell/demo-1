ddb2df32c6b3401bf09b4da12bb2cfd8ed860a83
andrtell <116459057+andrtell@users.noreply.github.com.>
2024-08-07 11:04:34 +0200

    wip

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -12,6 +12,23 @@ class List {
     }
     return seq;
   }
+
+  static normalize_min_max(lst) {
+    let min = lst[0];
+    let max = lst[0];
+    for (let i = 0; i < lst.length; i++) {
+      min = Math.min(min, lst[i]);
+      max = Math.max(max, lst[i]);
+    }
+    const range = max - min;
+    if (range == 0) {
+      return values;
+    }
+    for (let i = 0; i < lst.length; i++) {
+      lst[i] = (lst[i] - min) / range;
+    }
+    return values;
+  }
 }
 
 class Interpolation {
@@ -21,8 +38,8 @@ class Interpolation {
 
   static linear_2D(a, b, c, d, x_mu, y_mu) {
     const { linear_1D } = Interpolation;
-    y0 = linear_1D(a, b, x_mu);
-    y1 = linear_1D(c, d, x_mu);
+    const y0 = linear_1D(a, b, x_mu);
+    const y1 = linear_1D(c, d, x_mu);
     return linear_1D(y0, y1, y_mu);
   }
 
@@ -33,8 +50,8 @@ class Interpolation {
 
   static cosine_2D(a, b, c, d, x_mu, y_mu) {
     const { cosine_1D } = Interpolation;
-    y0 = cosine_1D(a, b, x_mu);
-    y1 = cosine_1D(c, d, x_mu);
+    const y0 = cosine_1D(a, b, x_mu);
+    const y1 = cosine_1D(c, d, x_mu);
     return cosine_1D(y0, y1, y_mu);
   }
 }
@@ -76,43 +93,43 @@ class Random {
     };
   }
 
-  static discrete_periodic_1D(count) {
-    const ns = Random.numbers(count);
-    const p1 = Random.permutation_1D(count);
+  static discrete_periodic_1D(no_samples) {
+    const ns = Random.numbers(no_samples);
+    const p1 = Random.permutation_1D(no_samples);
     return (x) => {
       return ns[p1(x)];
     };
   }
 
-  static discrete_periodic_2D(count) {
-    const ns = Random.numbers(count);
-    const p2 = Random.permutation_2D(count);
+  static discrete_periodic_2D(no_samples) {
+    const ns = Random.numbers(no_samples);
+    const p2 = Random.permutation_2D(no_samples);
     return (x, y) => {
       return ns[p2(x, y)];
     };
   }
 
-  static continuous_periodic_1D(count) {
-    const dis = Random.discrete_periodic_1D(count);
+  static continuous_periodic_1D(no_samples) {
+    const d1 = Random.discrete_periodic_1D(no_samples);
     return (x) => {
       const x_ = Math.floor(x);
       const mu = x - x_;
-      return Interpolation.linear_1D(dis(x_), dis(x_ + 1), mu);
+      return Interpolation.linear_1D(d1(x_), d1(x_ + 1), mu);
     };
   }
 
-  static continuous_periodic_2D(count) {
-    const dis = Random.discrete_periodic_2D(count);
+  static continuous_periodic_2D(no_samples) {
+    const d2 = Random.discrete_periodic_2D(no_samples);
     return (x, y) => {
       const x_ = Math.floor(x);
       const y_ = Math.floor(y);
       const x_mu = x - x_;
       const y_mu = y - y_;
       return Interpolation.linear_2D(
-        dis(x_, y_),
-        dis(x_ + 1, y_),
-        dic(x_, y_ + 1),
-        dis(x_ + 1, y_ + 1),
+        d2(x_, y_),
+        d2(x_ + 1, y_),
+        d2(x_, y_ + 1),
+        d2(x_ + 1, y_ + 1),
         x_mu,
         y_mu,
       );
@@ -120,64 +137,34 @@ class Random {
   }
 }
 
-n2 = Random.continuous_periodic_1D(100);
-console.log(n2(20, 20));
-console.log(n2(21, 20));
-console.log(n2(21, 100));
-
-class Normalize {
-  static min_max(values) {
-    let min = values[0];
-    let max = values[0];
-    for (let i = 0; i < values.length; i++) {
-      min = Math.min(min, values[i]);
-      max = Math.max(max, values[i]);
-    }
-    const range = max - min;
-    if (range == 0) {
-      return values;
-    }
-    for (let i = 0; i < values.length; i++) {
-      values[i] = (values[i] - min) / range;
-    }
-    return values;
-  }
-}
-
-class Scale {
-  static up(x, min, max) {
-    return x * (max - min) + min;
-  }
-}
-
-class Noise {
-  static make(octaves = 1) {
-    if (octaves < 1) {
-      octaves = 1;
-    }
-    const samples = [];
-    const freq_inc = 3;
-    for (let i = 0; i < octaves; i++) {
-      let f = freq_inc * (i + 1);
-      samples[i] = [f, Normalize.min_max(Random.numbers(f))];
-    }
-    return (x) => {
-      let y = 0;
-      let amp = 0.5;
-      for (let i = 0; i < samples.length; i++) {
-        let f = samples[i][0];
-        const xs = Scale.up(x, 0, f);
-        const x_ = Math.floor(xs);
-        const mu = xs - x_;
-        const s0 = samples[i][1][x_ % f];
-        const s1 = samples[i][1][(x_ + 1) % f];
-        y += Interpolation.linear_1D(s0, s1, mu) * amp;
-        amp /= 2;
-      }
-      return y;
-    };
-  }
-}
+// class Noise {
+//   static make(octaves = 1) {
+//     if (octaves < 1) {
+//       octaves = 1;
+//     }
+//     const samples = [];
+//     const freq_inc = 3;
+//     for (let i = 0; i < octaves; i++) {
+//       let f = freq_inc * (i + 1);
+//       samples[i] = [f, Normalize.min_max(Random.numbers(f))];
+//     }
+//     return (x) => {
+//       let y = 0;
+//       let amp = 0.5;
+//       for (let i = 0; i < samples.length; i++) {
+//         let f = samples[i][0];
+//         const xs = Scale.up(x, 0, f);
+//         const x_ = Math.floor(xs);
+//         const mu = xs - x_;
+//         const s0 = samples[i][1][x_ % f];
+//         const s1 = samples[i][1][(x_ + 1) % f];
+//         y += Interpolation.linear_1D(s0, s1, mu) * amp;
+//         amp /= 2;
+//       }
+//       return y;
+//     };
+//   }
+// }
 
 const can = document.getElementById("canvas");
 const bcr = can.getBoundingClientRect();
@@ -187,21 +174,20 @@ const ctx = can.getContext("2d");
 const dat = ctx.createImageData(bcr.width, bcr.height);
 
 // const x = create_n(10, can.width);
-const y = Noise.make(5);
+const z = Random.continuous_periodic_2D(10);
 
-const NO_PIX = can.width;
 const pix = [];
 
-for (i = 0; i < NO_PIX; i++) {
-  pix[i] = [i, Math.floor(Scale.up(y((1 / NO_PIX) * i), 0, can.width))];
-}
+console.log(z(10, 10));
 
-for (i = 0; i < NO_PIX; i++) {
-  let idx = (pix[i][0] + pix[i][1] * dat.width) * 4;
-  dat.data[idx + 0] = 180;
-  dat.data[idx + 1] = 180;
-  dat.data[idx + 2] = 180;
-  dat.data[idx + 3] = 255;
+for (let x = 0; x < can.width; x++) {
+  for (let y = 0; y < can.height; y++) {
+    let idx = (x + y * dat.width) * 4;
+    dat.data[idx + 0] = z(x * 0.1, y * 0.01) * 255;
+    dat.data[idx + 1] = z(x * 0.1, y * 0.001) * 255;
+    dat.data[idx + 2] = z(x * 0.1, y * 0.1) * 255;
+    dat.data[idx + 3] = 255;
+  }
 }
 //
 ctx.putImageData(dat, 0, 0);
